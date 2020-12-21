14c07a8cf8a294a81848976c7c7fc463d84fe5e9
uncle_sam <bwsamuel6@gmail.com>
2020-12-21 08:08:14 +0200

    Update add_house.html, multi-steps-form.js, multi-steps-form.scss

--- js/multi-steps-form.js
+++ js/multi-steps-form.js
@@ -1,6 +1,33 @@
 let currentStep = 0;
+let form = document.querySelector('.multi-steps');
+let prevBtn = document.getElementById("prevStepBtn");
+let nextBtn = document.getElementById("nextStepBtn");
+let steps = document.querySelectorAll('.multi-steps .step');
 showStep(currentStep);
-
 function showStep(n){
-    alert("Hello")
+    steps[n].classList.add('active');
+    /*Le bouton retour est désactivé lorsque on est à la prémière étape */
+    if(currentStep>0){
+        prevBtn.removeAttribute('disabled');
+    }else{
+        prevBtn.setAttribute('disabled', true);    
+    }
+    /*Si on est à la dernière étape*/
+    if(n == (steps.length - 1)){
+        nextBtn.innerHTML = "Confirmer";
+    }else{
+        nextBtn.innerHTML = "Suivant";
+    }
 }
+function nextPrev(n){
+    steps[currentStep].classList.remove('active');
+    currentStep = currentStep + n;
+    if(currentStep == steps.length) {
+        form.submit();
+        return false;
+    }
+    showStep(currentStep);
+    return false;
+}
+// prevBtn.addEventListener('click', nextPrev(-1));
+// nextBtn.addEventListener('click', nextPrev(1));
